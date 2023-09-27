22415ba6da075d9be7187c54d6ea973b7908e112
Utsav Shrestha <utsavshrestha1500@gmail.com>
2023-09-27 14:57:13 +0545

    my project

---
+++ style.js
@@ -0,0 +1,13 @@
+const buttonStyle= {
+    color:"#0cd8efe6",
+    backgroundColor: "white",
+    textAlign: "center",
+    margin: " 10px",
+    borderRadius: "20px",
+    padding: "6px",
+    fontSize: " 17px",
+    fontFamily: "Arial, Helvetica, sans-serif",
+    fontWeight: "bold",
+
+};
+export { buttonStyle };
